264407b0db853a845c454e36fe838672a7351c4e
IcyOreo226 <kevinjin@aurora-schools.org>
2020-01-06 18:48:33 -0500

    Created MoveLeft and MoveRight autonomous programs
    :D

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MoveRight.java
@@ -0,0 +1,59 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+//@Disabled
+@Autonomous(name="Move: Right", group="Test")
+public class MoveRight extends OpMode{
+
+    private int stateMachineFlow;
+    MecanumDrive robot = new MecanumDrive();
+    Intake intake      = new Intake();
+    Lift lift          = new Lift();
+    Placing placing    = new Placing();
+    SkystoneCam cam    = new SkystoneCam();
+
+    double time;
+    static final int PLATE_IS_MOVED = 1;
+    double distanceToTarget;
+    static final int NINETY_DEGREES = 90;
+    int angleToTarget = 0;
+    int initView = 0;
+    private ElapsedTime     runtime = new ElapsedTime();
+    /***********************************
+     *
+     * This program starts with the robot's intake facing the wall
+     *
+     ***********************************/
+    @Override
+    public void init() {
+        msStuckDetectInit = 11500;
+        msStuckDetectLoop = 10000;
+
+        robot.init(hardwareMap);
+        intake.init(hardwareMap);
+        lift.init(hardwareMap);
+        placing.init(hardwareMap);
+        //cam.init(hardwareMap);
+
+        stateMachineFlow = 0;
+    }
+
+    @Override
+    public void loop() {
+        switch (stateMachineFlow) {
+            case 0:
+                runtime.reset();
+                time = getRuntime();
+                stateMachineFlow++;
+                break;
+            case 1:
+                //move under the bridge
+                robot.sideDrive(.65,25);
+                stateMachineFlow++;
+                break;
+        }
+    }
+}
